6b88318ab7467ec8a471d21aa59b23ef78682c90
wyferic <yi.feng.eric.wang@gmail.com>
2025-09-29 21:53:35 -0400

    plz work

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ export const About = () => {
 	return (
 		<section id="about" className="scroll-mt-[96px] w-[90vw] md:w-[md] max-w-[60ch] bg-white dark:bg-gray-900 dark:text-white">
 			<h2>About</h2>
-			<img src="public/peak.png" className="my-4 mx-auto block rounded 2xl"></img>
+			<img src="/public/peak.png" className="my-4 mx-auto block rounded 2xl"></img>
 			<p>
 				{date}M, <del>ChatGPT enjoyer</del> "programmer."
 			</p>
