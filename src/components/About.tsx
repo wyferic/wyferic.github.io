fd415a25de66da57d0d6543d9db2bea9f8a5272e
wyferic <yi.feng.eric.wang@gmail.com>
2025-09-30 18:12:20 -0400

    ree

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,7 @@
 export const About = () => {
-	const date: number = Math.floor((new Date().getTime() - new Date(import.meta.env.BIRTHDAY + "T00:00:00Z").getTime()) / 31557600000);
+	const bday = import.meta.env.BIRTHDAY;
+	console.log("bday", bday)
+	const date: number = Math.floor((new Date().getTime() - new Date(bday + "T00:00:00Z").getTime()) / 31557600000);
 	return (
 		<section id="about" className="scroll-mt-[96px] w-[90vw] md:w-[md] max-w-[60ch] bg-white dark:bg-gray-900 dark:text-white">
 			<h2>About</h2>
