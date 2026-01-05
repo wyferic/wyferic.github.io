f40d9a91dfa8297d9f29175bf92f00b51f5252cc
wyferic <yi.feng.eric.wang@gmail.com>
2026-01-05 15:16:29 -0500

    temp fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
 "use client"
 
-import Home from "./pages/home/page";
+// import Home from "./pages/home/page";
 
-export default function Main() {
-  return Home();
+// export default function Main() {
+//   return Home();
+// }
+
+import { HeaderHome } from "@/app/_components/header";
+import { Background } from "@/app/pages/home/_components/background";
+import { Projects } from "@/app/pages/home/_components/projects";
+import { Contact } from "@/app/pages/home/_components/contact";
+import { Footer } from "@/app/pages/home/_components/footer";
+import { About } from "@/app/pages/home/_components/about";
+
+export default function Home() {
+  return (
+    <>
+      <HeaderHome />
+      <Background />
+      <div className="absolute w-screen top-12 min-h-[calc(100vh-48px)]">
+        <About />
+        <Projects />
+        <section className="drop-shadow-md">
+          <Contact />
+        </section>
+        <Footer />
+      </div>
+    </>
+  );
 }
